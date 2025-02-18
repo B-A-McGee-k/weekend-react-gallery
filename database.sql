c6d21bd3c012ed7f3634f778b54cca460c65dac9
Bryan McGee <mcgee.bryan.andrew@gmail.com>
2025-02-18 17:33:06 -0600

    Added more images

--- database.sql
+++ database.sql
@@ -10,5 +10,7 @@ INSERT INTO "gallery"
 ("url", "title", "description")
 VALUES
 ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
+('images/Happy-Goat.jpg', 'Silly Guy!', 'Photo of a goat smiling from donbrobst.com'),
+('images/Not-a-Goat.jpg', 'NOT A GOAT!', 'BEWARE OF THIS SO CALLED GOAT, from southdakotamagazine.com'),
 ('images/goat_stache.png', 'Goat Stache!', 'Photo of a mustachioed goat taken at Glacier National Park.');
   
